54268af787291833fbaa76dd4f2e7ea6ab947622
hi-anki <managemee@protonmail.com>
2025-09-12 21:07:16 +0530

    working perfect

--- 4-dda.c
+++ 4-dda.c
@@ -5,9 +5,10 @@
 
 void main() {
     int gd = DETECT, gm;
-    int x1, y1, x2, y2, color = 15;
+    int x1, y1, x2, y2;
     float dx, dy, steps, x_inc, y_inc;
     float x, y;
+    int k;
 
     initgraph(&gd, &gm, "C:\\BGI");
 
@@ -20,10 +21,10 @@ void main() {
     dx = x2 - x1;
     dy = y2 - y1;
 
-    if (fabs(dx) > fabs(dy)) {
-        steps = fabs(dx);
+    if (abs(dx) > abs(dy)) {
+        steps = abs(dx);
     } else {
-        steps = fabs(dy);
+        steps = abs(dy);
     }
 
     x_inc = dx / steps;
@@ -32,12 +33,12 @@ void main() {
     x = x1;
     y = y1;
 
-    putpixel(x, y, color);
+    putpixel(x, y, 15);
 
-    for (int k = 1; k <= steps; k++) {
+    for (k = 1; k <= steps; k++) {
         x = x + x_inc;
         y = y + y_inc;
-        putpixel(x, y, color);
+        putpixel(x, y, 15);
     }
 
     getch();
