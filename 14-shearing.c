6547e140d0954734cfba6af02b6812d092d770f3
hi-anki <managemee@protonmail.com>
2025-09-12 21:31:17 +0530

    working perfect

--- 14-shearing.c
+++ 14-shearing.c
@@ -9,25 +9,25 @@ void main() {
 
     initgraph(&gd, &gm, "C:\\BGI");
 
-    printf("Enter x1, y1: ");
+    printf("Enter (x1, y1): ");
     scanf("%d %d", &x1, &y1);
 
-    printf("Enter x2, y2: ");
+    printf("Enter (x2, y2): ");
     scanf("%d %d", &x2, &y2);
 
-    printf("Enter x3, y3: ");
+    printf("Enter (x3, y3): ");
     scanf("%d %d", &x3, &y3);
 
     printf("Enter shearing factors (shx, shy): ");
     scanf("%f %f", &shx, &shy);
 
-    // Draw original triangle
+    printf("Original triangle in white\n");
     setcolor(WHITE);
     line(x1, y1, x2, y2);
     line(x2, y2, x3, y3);
     line(x3, y3, x1, y1);
 
-    // Shear and draw new triangle
+    printf("Sheared triangle in yellow\n");
     setcolor(YELLOW);
     line(x1 + shx * y1, y1 + shy * x1, x2 + shx * y2, y2 + shy * x2);
     line(x2 + shx * y2, y2 + shy * x2, x3 + shx * y3, y3 + shy * x3);
