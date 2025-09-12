5be47ec91a2d217edc61146f635b8b06682e2c7c
Ankur Agrawal <managemee@protonmail.com>
2025-09-12 17:59:55 +0530

    Create 7-bresenham-circle.c

---
+++ 7-bresenham-circle.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+
+void main() {
+    int gd = DETECT, gm;
+    int xc, yc, r;
+    int x, y, d;
+
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+    printf("Enter center coordinates (xc, yc): ");
+    scanf("%d %d", &xc, &yc);
+    printf("Enter radius r: ");
+    scanf("%d", &r);
+
+    x = 0;
+    y = r;
+    d = 3 - 2 * r;
+
+    while (x <= y) {
+        putpixel(xc + x, yc + y, 15);
+        putpixel(xc - x, yc + y, 15);
+        putpixel(xc + x, yc - y, 15);
+        putpixel(xc - x, yc - y, 15);
+        putpixel(xc + y, yc + x, 15);
+        putpixel(xc - y, yc + x, 15);
+        putpixel(xc + y, yc - x, 15);
+        putpixel(xc - y, yc - x, 15);
+
+        x++;
+        if (d < 0) {
+            d = d + 4 * x + 6;
+        } else {
+            y--;
+            d = d + 4 * (x - y) + 10;
+        }
+    }
+
+    getch();
+    closegraph();
+}
