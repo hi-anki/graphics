4a318ccc393d35381a6297d11df0b1dce517711e
hi-anki <managemee@protonmail.com>
2025-09-12 21:33:45 +0530

    working perfect

--- 15-reflection.c
+++ 15-reflection.c
@@ -9,13 +9,13 @@ void main() {
 
     initgraph(&gd, &gm, "C:\\BGI");
 
-    printf("Enter x1, y1: ");
+    printf("Enter (x1, y1): ");
     scanf("%d %d", &x1, &y1);
 
-    printf("Enter x2, y2: ");
+    printf("Enter (x2, y2): ");
     scanf("%d %d", &x2, &y2);
 
-    printf("Enter x3, y3: ");
+    printf("Enter (x3, y3): ");
     scanf("%d %d", &x3, &y3);
 
     printf("Choose reflection axis:\n");
@@ -23,7 +23,7 @@ void main() {
     printf("2. Y-axis\n");
     scanf("%d", &choice);
 
-    // Draw original triangle
+    printf("Original triangle in white\n");
     setcolor(WHITE);
     line(x1, y1, x2, y2);
     line(x2, y2, x3, y3);
@@ -31,11 +31,13 @@ void main() {
 
     // Reflect and draw new triangle
     setcolor(YELLOW);
-    if (choice == 1) { // Reflection about X-axis
+    if (choice == 1) {
+        printf("Reflected triangle about x-axis in yellow\n");
         line(x1, getmaxy() - y1, x2, getmaxy() - y2);
         line(x2, getmaxy() - y2, x3, getmaxy() - y3);
         line(x3, getmaxy() - y3, x1, getmaxy() - y1);
-    } else if (choice == 2) { // Reflection about Y-axis
+    } else if (choice == 2) {
+        printf("Reflected triangle about y-axis in yellow\n");
         line(getmaxx() - x1, y1, getmaxx() - x2, y2);
         line(getmaxx() - x2, y2, getmaxx() - x3, y3);
         line(getmaxx() - x3, y3, getmaxx() - x1, y1);
