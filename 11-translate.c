e9c0c73a68c960448a072db82ea0cbf732fe4538
Ankur Agrawal <managemee@protonmail.com>
2025-09-12 18:01:36 +0530

    Update and rename 1-translate.c to 11-translate.c

---
+++ 11-translate.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+
+void main() {
+    int gd = DETECT, gm;
+    int x1, y1, x2, y2, x3, y3;
+    int tx, ty;
+
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+    printf("Enter triangle coordinates (x1,y1), (x2,y2), (x3,y3):\n");
+    scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
+
+    printf("Enter translation factors (tx, ty): ");
+    scanf("%d %d", &tx, &ty);
+
+    // Draw original triangle
+    setcolor(WHITE);
+    line(x1, y1, x2, y2);
+    line(x2, y2, x3, y3);
+    line(x3, y3, x1, y1);
+
+    // Translate and draw new triangle
+    setcolor(YELLOW);
+    line(x1 + tx, y1 + ty, x2 + tx, y2 + ty);
+    line(x2 + tx, y2 + ty, x3 + tx, y3 + ty);
+    line(x3 + tx, y3 + ty, x1 + tx, y1 + ty);
+
+    getch();
+    closegraph();
+}
