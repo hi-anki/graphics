080631f4980e78b89f22d65a9994789eedeb77db
hi-anki <managemee@protonmail.com>
2025-09-12 22:07:47 +0530

    working perfect

--- 3-line-simple-method.c
+++ 3-line-simple-method.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <conio.h>
 #include <graphics.h>
+#include <math.h>
 
 void main() {
     int gd = DETECT, gm;
-    int x1, y1, x2, y2, color = 15;
-    float x_step, y_step;
+    int x1, y1, x2, y2, dx, dy, color = 15;
+    float x_step, y_step, x, y;
     int i, steps;
 
     initgraph(&gd, &gm, "C:\\BGI");
@@ -16,8 +17,8 @@ void main() {
     printf("Enter x2, y2: ");
     scanf("%d %d", &x2, &y2);
 
-    int dx = x2 - x1;
-    int dy = y2 - y1;
+    dx = x2 - x1;
+    dy = y2 - y1;
 
     if (abs(dx) > abs(dy)) {
         steps = abs(dx);
@@ -27,11 +28,11 @@ void main() {
 
     x_step = (float)dx / steps;
     y_step = (float)dy / steps;
+    
+    x = x1;
+    y = y1;
 
-    float x = x1;
-    float y = y1;
-
-    for (i = 0; i < steps; i++) {
+    for (i = 0; i <= steps; i++) {
         putpixel((int)(x + 0.5), (int)(y + 0.5), color);
         x += x_step;
         y += y_step;
